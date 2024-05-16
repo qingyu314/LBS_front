09b3b95518d4038ccf83e080f2a91396bd6c4b99
麦超禹 <1677425105@qq.com>
2024-05-16 17:32:08 +0800

    modify css

--- src/views/userDetail.vue
+++ src/views/userDetail.vue
@@ -1,43 +1,45 @@
 <template>
-  <div style="width: 100%; overflow: hidden;">
-    <el-card style="width: 60%; margin: 20px auto; border-radius: 8px">
-      <div class="profile-card">
-        <div class="profile-header">
-          <img class="cover-photo" src="/profile_2.png" alt="Cover Photo"/>
+  <el-card style="width: 60%; margin: 20px auto; border-radius: 8px">
+    <template #header>
+      <div class="profile-header">
+        <img class="cover-photo" src="/profile_1.png" alt="Cover Photo"/>
+      </div>
+      <div class="profile-content">
+        <el-icon :size="80">
+          <User/>
+        </el-icon>
+        <div class="user-info">
+          <h1 class="username">{{ form.username || '未登录' }}</h1>
+          <span>{{ form.introduction || '暂无个人介绍' }}</span>
         </div>
-        <div class="profile-content">
-          <el-icon :size="80">
-            <User/>
-          </el-icon>
-          <div class="user-info">
-            <h1 class="username">{{ form.username || '未登录' }}</h1>
-            <span>{{ form.introduction || '暂无个人介绍' }}</span>
+      </div>
+    </template>
+    <div class="image-record">
+      <div class="image-container" v-for="image in images" :key="image.id">
+        <el-card shadow="hover" body-style="padding: 10px" @mouseover="showButtons(image.id)"
+                 @mouseleave="hideButtons(image.id)">
+          <img :src="image.url" :alt="'Image ' + image.id" class="image"/>
+          <div class="overlay" v-if="image.showButtons">
+            <el-button type="primary" plain :icon="Position" size="large" @click="goToMap(image)" class="cover-button"/>
           </div>
-        </div>
+        </el-card>
       </div>
-      <div class="image-record">
-        <div class="image-container" v-for="image in images" :key="image.id">
-          <el-card style="border-radius: 8px">
-            <img :src="image.url" :alt="'Image ' + image.id" class="image"/>
-          </el-card>
-        </div>
-      </div>
-    </el-card>
-  </div>
-  <div class="footer">
-    <img src="/footer.png" alt="Footer Image" class="footer-image"/>
-  </div>
+    </div>
+    <div class="footer">
+      <img src="/footer.png" alt="Footer Image" class="footer-image"/>
+    </div>
+  </el-card>
 </template>
 
 <script setup>
-import {ref, onMounted, reactive} from 'vue';
+import {User, Position} from "@element-plus/icons-vue";
+import {reactive, ref, inject, onMounted} from 'vue';
 import {ElMessage} from 'element-plus';
 import request from "@/utils/request";
-import {useRoute} from 'vue-router';
-import {User} from "@element-plus/icons-vue";
+import {useRouter} from 'vue-router';
 
 let images = ref([]);
-const route = useRoute();
+const route = useRouter();
 let form = reactive({
   username: '未知用户',
   introduction: '暂无个人介绍',
@@ -95,13 +97,34 @@ function loadImages(userId) {
     });
   });
 }
+
+function showButtons(imageId) {
+  const image = images.value.find(img => img.id === imageId);
+  if (image) {
+    image.showButtons = true;
+  }
+}
+
+function hideButtons(imageId) {
+  const image = images.value.find(img => img.id === imageId);
+  if (image) {
+    image.showButtons = false;
+  }
+}
+
+function goToMap(image) {
+  router.push({
+    path: '/map',
+    query: {
+      id: image.id,
+      lat: image.lat,
+      lon: image.lon,
+    }
+  });
+}
 </script>
 
 <style scoped>
-.profile-card {
-  width: 100%;
-}
-
 .profile-header {
   position: relative;
   height: 350px;
@@ -140,6 +163,7 @@ function loadImages(userId) {
 }
 
 .image-container {
+  position: relative;
   text-align: center;
   border-radius: 16px;
   grid-row-start: auto;
@@ -150,9 +174,35 @@ function loadImages(userId) {
 .image {
   width: 100%;
   height: auto;
-  border: 1px solid #ccc;
   border-radius: 4px;
-  padding: 5px;
+}
+
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(145deg, rgba(0, 0, 0, 0.05), rgba(0, 0, 0, 0.1));
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  padding-top: 15px;
+  padding-right: 20px;
+  gap: 10px;
+  opacity: 0;
+  border-radius: 4px;
+}
+
+.image-container:hover .overlay {
+  opacity: 1;
+}
+
+.cover-button {
+  width: 2vw;
+  height: 2vw;
+  border-radius: 25%;
+  opacity: 0.75;
 }
 
 .footer {
@@ -161,6 +211,6 @@ function loadImages(userId) {
 }
 
 .footer-image {
-  width: 25%;
+  width: 40%;
 }
 </style>
